1c1319ac152f238de4045d23cf00535a11daa269
Somya Mahapatra <ritul.mahapatra@gmail.com>
2020-07-31 15:03:04 +0530

    Update Google_image_scraper.py

--- Google_image_scraper.py
+++ Google_image_scraper.py
@@ -49,7 +49,7 @@ for urls in image_urls:
             image = Image.open(image_file).convert('RGB')
             file_path=os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10]+'.jpg')
             with open(file_path , 'wb') as f:
-                f.write(imgdata)
+                image.save(f, "JPEG", quality=85)
                 print('Successfully Saved')
         except Exception as e:
             print("Couldnt be saved")
