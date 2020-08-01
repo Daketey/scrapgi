ac449f0bacb8ef33d84750ffccf22e6202388863
Somya Mahapatra <ritul.mahapatra@gmail.com>
2020-08-01 08:55:07 +0530

    Update Google_image_scraper.py

--- Google_image_scraper.py
+++ Google_image_scraper.py
@@ -1,30 +1,31 @@
-from bs4 import BeautifulSoup
 import selenium
 from selenium import webdriver
 import time
+import base64
 import requests
 import os
 import io
 from PIL import Image
 import hashlib
-import base64
 
-driver_path = 'C:/project/chromedriver'
-web_driv = webdriver.Chrome(executable_path = driver_path)
-
-search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q=cats&oq=cats&gs_l=img"    ##Replace cats with anything you want to collect images for
-web_driv.get(search_url)
-
-image_urls = set()
-image_count = 0
-number_results = 0
-
-def scroll_to_end(wd, scroll_point):
+def scroll_to_end(web_driv, scroll_point):
     web_driv.execute_script(f"window.scrollTo(0, {scroll_point});")
     time.sleep(1)
+
+def CallFromBrowser(Search_word , Driver_path ):
     
-for i in range(1,20):
-        scroll_to_end(web_driv, i*1000)
+    web_driv = webdriver.Chrome(executable_path = Driver_path)
+
+    search_url = f"https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={Search_word}s&oq={Search_word}&gs_l=img"
+
+    web_driv.get(search_url)
+
+    image_urls = set()
+    image_count = 0
+    number_results = 0
+
+    for i in range(1,20):
+        scroll_to_end(web_driv, i*100)
         time.sleep(5)
         thumb = web_driv.find_elements_by_css_selector("img")
         for img in thumb:
@@ -34,45 +35,42 @@ for i in range(1,20):
             image_count = len(image_urls)
             number_results = image_count
             time.sleep(.5)
-        print(f"Found: {number_results} search results")
-        
+        print(f"Found: {number_results} search results. Extracting links...")
 
-folder_path = 'C:/project/Scraper'
+    return image_urls
 
+def Save_Image(folder_path , image_urls):
 
-for urls in image_urls:
-    try:
-        image_content = requests.get(urls).content
-        print("Successfully downloaded")   
+    for urls in image_urls:
         try:
-            image_file = io.BytesIO(image_content)
-            image = Image.open(image_file).convert('RGB')
-            file_path=os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10]+'.jpg')
-            with open(file_path , 'wb') as f:
-                image.save(f, "JPEG", quality=85)
-                print('Successfully Saved')
-        except Exception as e:
-            print("Couldnt be saved")
-        
-        
-    except Exception as e:
-        try:
-            urls = urls.split(',')[1]
-            base64_bytes = urls.encode('ascii')
-            imgdata = base64.b64decode(urls)
-            filename = f'C:/games/Scraper/{hashlib.sha1(base64_bytes).hexdigest()[:10]}.jpg'
-            with open(filename, 'wb') as f:
-                 f.write(imgdata)
-            print("Successfully downloaded and Saved")
+            image_content = requests.get(urls).content
+            print("Successfully downloaded")
+
+            try:
+                image_file = io.BytesIO(image_content)
+                image = Image.open(image_file).convert('RGB')
+                file_path=os.path.join(folder_path,hashlib.sha1(image_content).hexdigest()[:10]+'.jpg')
+                with open(file_path , 'wb') as f:
+                    image.save(f, "JPEG", quality=85)
+                    print('Successfully Saved')
             
-        except Exception as e:
-            print("Cannot Download")           
-       
-   
-        
-     web_driv.quit()
-        
-    
-        
+            except:
+                print("Couldnt be saved")  
 
+
+        except: 
+            try:
+                urls = urls.split(',')[1]
+                base64_bytes = urls.encode('ascii')
+                imgdata = base64.b64decode(urls)
+                filename = f'{folder_path}/{hashlib.sha1(base64_bytes).hexdigest()[:10]}.jpg'
+
+                with open(filename, 'wb') as f:
+                 f.write(imgdata)
+                print("Successfully downloaded and Saved")  
+            
+            except:
+                print("Cannot Download")   
+                    
+            
 
